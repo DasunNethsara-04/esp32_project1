b2a93216bfbc64536a21ae707902f8e4ac7d4fb3
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-03-18 22:20:18 +0530

    Enhance LED control API with endpoints for all LEDs and individual LED state management

--- backend/app.py
+++ backend/app.py
@@ -15,9 +15,25 @@ app.add_middleware(
 )
 
 
-@app.get("/led/{state}")
-async def control_led(state: str) -> dict[str, str]:
+@app.get("/led/all/{state}")
+async def control_all_leds(state: str) -> dict[str, str]:
     if state.upper() not in ['ON', 'OFF']:
         return {"error": "Invalid state. 'ON' or 'OFF' only"}
-    response = requests.get(f"{ESP_IP}/led?state={state.upper()}")
+    response: requests.Response = requests.get(f"{ESP_IP}/led/all?state={state.upper()}")
+    return {"message": response.text}
+
+
+@app.get("/led/pattern/{state}")
+async def toggle_display_led_pattern(state: str) -> dict[str, str]:
+    if state.upper() not in ['ON', 'OFF']:
+        return {"error": "Invalid state. 'ON' or 'OFF' only"}
+    response: requests.Response = requests.get(f"{ESP_IP}/led/pattern?state={state.upper()}")
+    return {"message": response.text}
+
+
+@app.get("/led/{led}/{state}")
+async def control_single_led(led: int, state: str) -> dict[str, str]:
+    if led not in [1, 2, 3, 4, 5] or state.upper() not in ['ON', 'OFF']:
+        return {"error": "Invalid LED or state."}
+    response: requests.Response = requests.get(f"{ESP_IP}/led?led={led}&state={state.upper()}")
     return {"message": response.text}
